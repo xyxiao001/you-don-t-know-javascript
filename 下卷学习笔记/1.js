379542142c51d312b84aa05d1ecaacc436aa1c37
”xyxiao001” <463618622@qq.com>
2018-03-10 22:12:39 +0800

    update 43

---
+++ 下卷学习笔记/1.js
@@ -0,0 +1,21 @@
+{
+  // while
+  let i = 5
+  let j = 0
+  while (i > 1) {
+    i--
+    j += i
+  }
+  console.log(j)
+}
+
+{
+  // while
+  let i = 5
+  let j = 0
+  do {
+    i--
+    j += i
+  } while (i > 1)
+  console.log(j)
+}
