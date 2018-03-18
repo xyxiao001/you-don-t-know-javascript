1748a4ff828ddc5bf9858661c8928a50040f22c7
xyxiao001 <463618622@qq.com>
2018-03-18 23:52:22 +0800

    update service work

--- pwa/example/sw.js
+++ pwa/example/sw.js
@@ -5,10 +5,22 @@
         scoped: '/'
       }).then((registration) => {
         console.log('success', registration)
+        this.addEventListener('install', (e) => {
+          e.waitUntil(
+            caches.open('my-test-cache-v1').then((cache) => {
+              return cache.addAll([
+                '/',
+                '/index.html',
+                'main.css',
+                'main.js',
+                '/image.jpg'
+              ])
+            })
+          )
+        })
       }).catch((error) => {
         console.log('fail', error)
       })
     })
   }
-  console.log(1)
 }())
