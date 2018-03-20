9fd0368c68ec940f259c68a13b90357b0537cf2c
”xyxiao001” <463618622@qq.com>
2018-03-20 18:46:22 +0800

    response

--- pwa/example/sw.js
+++ pwa/example/sw.js
@@ -5,22 +5,52 @@
         scoped: '/'
       }).then((registration) => {
         console.log('success', registration)
-        this.addEventListener('install', (e) => {
-          e.waitUntil(
-            caches.open('my-test-cache-v1').then((cache) => {
-              return cache.addAll([
-                '/',
-                '/index.html',
-                'main.css',
-                'main.js',
-                '/image.jpg'
-              ])
-            })
-          )
-        })
       }).catch((error) => {
         console.log('fail', error)
       })
+    })
+
+    self.addEventListener('install', (e) => {
+      e.waitUntil(
+        caches.open('my-test-cache-v1').then((cache) => {
+          return cache.addAll([
+            '/',
+            '/index.html',
+            'main.css',
+            'main.js',
+            '/image.jpg'
+          ])
+        }).catch((error) => {
+          console.log(error)
+        })
+      )
+    })
+
+    self.addEventListener('fetch', (e) => {
+      e.respondWith(
+        caches.match(e, request).then((response) => {
+          if (response) {
+            return response
+          }
+
+          var request = e.request.clone()
+          return fetch(request).then((httpRes) => {
+
+            //请求失败
+            if (!httpRes || httpRes.status !== 200) {
+              return httpRes
+            }
+
+            // 请求成功
+            var responseClone = httpRes.clone()
+            caches.open('my-test-cache-v1').then((cache) => {
+              cache.put(e.request, responseClone)
+            })
+
+            return httpRes
+          })
+        })
+      )
     })
   }
 }())
