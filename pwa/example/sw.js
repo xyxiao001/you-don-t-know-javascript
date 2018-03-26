945f7d12ddaec117a64ea53b9696c0b2577ebf81
”xyxiao001” <463618622@qq.com>
2018-03-26 14:41:44 +0800

    update

--- pwa/example/sw.js
+++ pwa/example/sw.js
@@ -4,6 +4,9 @@ var cacheNameTest = 'my-test-cache-v1'
 var ENV = 'development'
 
 self.addEventListener('install', (e) => {
+  if (ENV === 'development') {
+    self.skipWaiting()
+  }
   e.waitUntil(
     caches.open(cacheNameTest).then((cache) => {
       return cache.addAll([
@@ -15,9 +18,6 @@ self.addEventListener('install', (e) => {
       console.log(error)
     })
   )
-  if (ENV === 'development') {
-    self.skipWaiting()
-  }
 })
 
 self.addEventListener('active', (e) => {
