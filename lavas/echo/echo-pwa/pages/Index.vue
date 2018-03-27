8cfe938dea077132d57d7880b736b07d95449f6b
”xyxiao001” <463618622@qq.com>
2018-03-27 18:49:35 +0800

    add pwa echo

---
+++ lavas/echo/echo-pwa/pages/Index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="content">
+      <div>
+        <h2>echo</h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex';
+function setState(store) {
+    store.dispatch('appShell/appHeader/setAppHeader', {
+        show: true,
+        title: 'echo',
+        showMenu: true,
+        showBack: false,
+        showLogo: false,
+        actions: [
+            {
+                icon: 'search',
+                route: '/search'
+            }
+        ]
+    });
+}
+
+export default {
+    name: 'index',
+    metaInfo: {
+        title: 'Home',
+        titleTemplate: 'echo',
+        meta: [
+            {name: 'keywords', content: 'echo'},
+            {name: 'description', content: 'echo 首页'}
+        ]
+    },
+    async asyncData({store, route}) {
+        setState(store);
+    },
+    activated() {
+        setState(this.$store);
+    }
+};
+</script>
+
+<style lang="stylus" scoped>
+.content
+    display flex
+    align-items center
+    justify-content center
+    height 100%
+    flex-wrap wrap
+    h2
+        font-size 46px
+        font-weight 500
+</style>
